bd8cd082166fbcef0d495361bb812c646f58b29e
alexlambertini <38925045+alexlambertini@users.noreply.github.com>
2018-05-09 00:52:22 -0300

    versao 0.0.2
    Melhoria de código
    
    Co-Authored-By: Charles Danillo <danillocharles@gmail.com>

--- trivia.py
+++ trivia.py
@@ -1,24 +1,124 @@
+import random
+
 name = input("Qual seu nome? ")
-decid = input("Vamos comecar o jogo? ").lower()
 
-if decid == ("sim"):
-    print("Entao vamos la \n")
-elif decid == ("claro"):
-    print("Entao vamos la \n")
-elif decid == ("ok"):
-    print("Entao vamos la \n")
-elif decid == ("s"):
-    print("Entao vamos la \n")
-else:
-    print("ok, entao bye")
-    exit()
+class Inicio:
+    def __init__(self):
+        decid = input("Vamos comecar o jogo? ").lower()
 
-print("\n===================================================\n Seja Bem Vindo %s \n===================================================\n" % (name))
+        if decid in ["sim", "claro", "ok", "s", "sure", "bora"]:
+            print("Entao vamos la\n")
 
-print("Qual a cor do cavalo branco de Napoleao? \n\n a) Branco \n b) Preto \n c) Vermelho \n")
-resp = input().lower()
-score = str("100")
-if resp == ("b"):
-    print("\nVoce escolheu a resposta %s \nAcertou Miresavi !!! \nVoce acumulou %s pontos" % (resp.upper(), score))
-else:
-    print("\nVoce escolheu a resposta %s, resposta errada!!" % (resp.upper()))
+        else:
+            print("ok, entao bye")
+            exit()
+
+print("""
+==========================================================================
+
+Seja Bem Vindo %s
+
+--------------------------------------------------------------------------
+
+Regras: A cada questao respondida corretamente voce acumula +100 pontos
+por outro lado a cada questao errada sera descontado -50 pontos
+
+Boa sorte !
+
+==========================================================================\n""" % (name))
+
+class Jogo:
+    pontuacao = 0
+
+    acerto = 100
+    erro = -50
+
+    ganhar = 400
+    perder = -300
+
+    perguntas = [
+        {
+            'pergunta': 'Qual a cor do cavalo branco de Napoleao?\n',
+            'respostas': {
+                'a': 'Branco',
+                'b': 'Preto',
+                'c': 'Vermelho',
+                'd': 'Azul\n'
+            },
+            'certa': 'b'
+        },
+        {
+            'pergunta': 'O UFC foi criado por quem?\n',
+            'respostas': {
+                'a': 'Helio Gracie',
+                'b': 'Vitor Belfort',
+                'c': 'Carlinhos Gracie\n'
+            },
+            'certa': 'c'
+        },
+        {
+            'pergunta': 'Aonde foi a famosa Baguncinha?\n',
+            'respostas': {
+                'a': 'Los Anges, California',
+                'b': 'New York, Albany',
+                'c': 'Brasil, Berlandia\n'
+            },
+            'certa': 'a'
+        },
+    ]
+
+    def __init__(self):
+        self.perguntar()
+
+    def perguntar(self):
+        pergunta = random.choice(self.perguntas)
+
+        texto_da_pergunta = pergunta['pergunta']
+        alternativas = pergunta['respostas'].keys()
+
+        while True:
+            print(texto_da_pergunta)
+            for alternativa in alternativas:
+                print("%s) %s" % (alternativa, pergunta['respostas'][alternativa]))
+
+            resposta = input()
+            if (resposta in alternativas):
+                print("\nVoce escolheu a resposta %s\n" % resposta.upper())
+                if (resposta == pergunta['certa']):
+                    self.acertou()
+                else:
+                    self.errou()
+
+                self.pontuacaoAtual()
+                break
+            else:
+                print("\nResposta invalida.\n")
+
+        if (self.pontuacao >= self.ganhar):
+            self.ganhou()
+        elif (self.pontuacao <= self.perder):
+            self.perdeu()
+        else:
+            self.perguntar()
+
+    def acertou(self):
+        print('Acertou Miresavi !!!')
+        self.pontuacao = self.pontuacao + self.acerto
+
+    def errou(self):
+        print('\nresposta errada!!')
+        self.pontuacao = self.pontuacao + self.erro
+
+    def pontuacaoAtual(self):
+        print('Voce acumulou %s pontos\n' % self.pontuacao)
+
+    def ganhou(self):
+        print('\nGANHOU !!!\n')
+        exit()
+
+    def perdeu(self):
+        print('\nPERDEU PREYBOI !!!\n')
+        exit()
+
+Inicio()
+jogo = Jogo()
