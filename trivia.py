7046fe6ddaaa83e5a6afa3ed0d3a9df53d02c783
alexlambertini <38925045+alexlambertini@users.noreply.github.com>
2018-05-08 21:56:45 -0300

    v.0.0.1

--- trivia.py
+++ trivia.py
@@ -1,15 +1,24 @@
 name = input("Qual seu nome? ")
 decid = input("Vamos comecar o jogo? ").lower()
+
 if decid == ("sim"):
+    print("Entao vamos la \n")
+elif decid == ("claro"):
+    print("Entao vamos la \n")
+elif decid == ("ok"):
+    print("Entao vamos la \n")
+elif decid == ("s"):
     print("Entao vamos la \n")
 else:
     print("ok, entao bye")
+    exit()
 
 print("\n===================================================\n Seja Bem Vindo %s \n===================================================\n" % (name))
 
 print("Qual a cor do cavalo branco de Napoleao? \n\n a) Branco \n b) Preto \n c) Vermelho \n")
-resp = input()
-if resp == ("a"):
-    print("\n Acertou Miresavi !! Voce acumulou " + str(100) + " pontos")
+resp = input().lower()
+score = str("100")
+if resp == ("b"):
+    print("\nVoce escolheu a resposta %s \nAcertou Miresavi !!! \nVoce acumulou %s pontos" % (resp.upper(), score))
 else:
-    print("\n Resposta errada")
+    print("\nVoce escolheu a resposta %s, resposta errada!!" % (resp.upper()))
